8ce5b5f4fd58dbc1f91bb8d314aa2954d47d7e76
Antonio <agm9581@gmail.com>
2026-05-14 18:35:46 +0200

    Init backend repo

---
+++ claim-management-backend/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/claims", (req: Request, res: Response) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
